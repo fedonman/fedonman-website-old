8073d53044e65b233c0f9f085139d84facd6306e
fedonman <fedonman@gmail.com>
2017-12-27 21:39:22 +0200

    adds august app

---
+++ public/js/august.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+	const container = document.getElementById('date');
+	const today = moment();
+	let lastAugust = '01/08/';
+	today.month() <= 6 ? lastAugust += today.year() - 1 : lastAugust += today.year();
+	const august = moment(lastAugust, 'DD/MM/YYYY');
+	const diff = today.diff(august, "days");
+	container.innerHTML = `Σήμερα είναι ${diff} Αυγούστου!`;
+}
