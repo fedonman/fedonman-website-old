9ad5fa1674b29a12ebacb37fd1edfe620c73141e
fedonman <fedonman@gmail.com>
2018-04-24 20:12:03 +0300

    deletes 'coming soon' website

--- public/js/august.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-	const container = document.getElementById('date');
-	const today = moment();
-	let lastAugust = '01/08/';
-	today.month() <= 6 ? lastAugust += today.year() - 1 : lastAugust += today.year();
-	const august = moment(lastAugust, 'DD/MM/YYYY');
-	const diff = today.diff(august, "days") + 1;
-	container.innerHTML = `Σήμερα είναι ${diff} Αυγούστου!`;
-}
