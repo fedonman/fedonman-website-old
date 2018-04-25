4d09c6b89b4d18ebd706ee85631e585bf3c61f32
fedonman <fedonman@gmail.com>
2018-04-25 04:31:41 +0300

    fixes mobile

--- public/js/main.js
+++ public/js/main.js
@@ -8,62 +8,90 @@ const init = () => {
 
     document.querySelectorAll("a[rel=external]").forEach(el => el.setAttribute("target", "_blank"));
 
-    const frames = 28;
-    let sectionCount = 0;
-    const sectionElements = Array.from(document.getElementsByClassName("repeated-section"));
-    const templateElements = [];
-    
-    sectionElements.forEach(el => {
-        templateElements.push(el.cloneNode(true));
-        const newClass = zeroPad(sectionCount, 2);
-        el.querySelector(".cat").classList.add("f"+newClass);
-        if(sectionCount < frames) {
-            sectionCount++;
-        }
-        else {
-            sectionCount = 0;
-        }
-    });
-    
-    const sectionHeight = sectionElements[0].clientHeight;
-    let scrollCount = 0;
-    let stillScrolling = true;
-
-    const scroll = () => {
-        if (stillScrolling) {
-            if (window.pageYOffset > document.body.scrollHeight - sectionHeight * 10) {
-                scrollCount++;
-                console.log(scrollCount);
-                const beginElement = document.getElementById("begin");
-                if (scrollCount >= 20) {
-                    const endElement = document.getElementById("end");
-                    endElement.style.display = "block";
-                    beginElement.appendChild(endElement);
-                    stillScrolling = false;
-                    return false;
-                }
-
-                console.log(sectionElements);
-                templateElements.forEach(el => {
-                    console.log(sectionCount + " - ");
-                    if(sectionCount < frames) {
-                        sectionCount++;
-                    }
-                    else {
-                        sectionCount = 0;
-                    }
-
-                    const newElement = el.cloneNode(true);
-                    const newClass = zeroPad(sectionCount, 2);
-                    newElement.querySelector(".cat").classList.add("f"+newClass);
-                    beginElement.appendChild(newElement);
-                });
+    const md = new MobileDetect(window.navigator.userAgent);
+    if (!md.mobile()) {
+        const frames = 28;
+        let sectionCount = 0;
+        const sectionElements = Array.from(document.getElementsByClassName("repeated-section"));
+        const templateElements = [];
+        
+        sectionElements.forEach(el => {
+            templateElements.push(el.cloneNode(true));
+            const newClass = zeroPad(sectionCount, 2);
+            el.querySelector(".cat").classList.add("f"+newClass);
+            if(sectionCount < frames) {
+                sectionCount++;
             }
-        }
-        return false;
-    };
+            else {
+                sectionCount = 0;
+            }
+        });
+        
+        const sectionHeight = sectionElements[0].clientHeight;
+        let scrollCount = 0;
+        let stillScrolling = true;
 
-    window.onscroll = scroll;
+        const scroll = () => {
+            if (stillScrolling) {
+                if (window.pageYOffset > document.body.scrollHeight - sectionHeight * 10) {
+                    scrollCount++;
+                    const beginElement = document.getElementById("begin");
+                    if (scrollCount >= 20) {
+                        const endElement = document.getElementById("end");
+                        endElement.style.display = "block";
+                        beginElement.appendChild(endElement);
+                        stillScrolling = false;
+                        return false;
+                    }
+
+                    templateElements.forEach(el => {
+                        if(sectionCount < frames) {
+                            sectionCount++;
+                        }
+                        else {
+                            sectionCount = 0;
+                        }
+
+                        const newElement = el.cloneNode(true);
+                        const newClass = zeroPad(sectionCount, 2);
+                        newElement.querySelector(".cat").classList.add("f"+newClass);
+                        beginElement.appendChild(newElement);
+                    });
+                }
+            }
+            return false;
+        };
+
+        window.onscroll = scroll;
+    }
+    else {
+        document.getElementsByTagName('body')[0].classList.add("mobile");
+        const sectionElements = Array.from(document.getElementsByClassName("repeated-section"));
+        const sectionHeight = sectionElements[0].clientHeight;
+        let scrollCount = 0;
+        let stillScrolling = true;
+        const scroll = () => {
+            if (stillScrolling) {
+                if (window.pageYOffset > document.body.scrollHeight - sectionHeight * 10) {
+                    scrollCount++;
+                    const beginElement = document.getElementById("begin");
+                    if (scrollCount >= 20) {
+                        const endElement = document.getElementById("end");
+                        endElement.style.display = "block";
+                        beginElement.appendChild(endElement);
+                        stillScrolling = false;
+                        return false;
+                    }
+                    sectionElements.forEach(el => {
+                        const newElement = el.cloneNode(true);
+                        beginElement.appendChild(newElement);
+                    });
+                }
+            }
+            return false;
+        };
+        window.onscroll = scroll;
+    }
 
     const mainElement = document.getElementById("content");
     mainElement.classList.add("fade-in");
